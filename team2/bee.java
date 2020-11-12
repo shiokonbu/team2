70487e78e3349f63a34c97537fed953f91e90258
釜石 健太郎 <16035@kauth.local>
2020-11-12 09:33:15 +0900

    Dがキャラ移動処理を追加

--- team2/bee.java
+++ team2/bee.java
@@ -15,5 +15,9 @@ public class bee extends Actor
     public void act() 
     {
         // Add your action code here.
+        if( Greenfoot.isKeyDown( "left" ) ){
+            setRotation(180);
+            move(1);
+        }
     }    
 }
