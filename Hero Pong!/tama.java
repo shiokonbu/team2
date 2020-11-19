0a6124dd2a123e31e024b874b318303d543b10f0
伊藤 一真 <16010@kauth.local>
2020-11-19 09:59:28 +0900

    tamaに修正

--- Hero Pong!/tama.java
+++ Hero Pong!/tama.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class maru extends Actor
+public class tama extends Actor
 {
     /**
      * Act - do whatever the maru wants to do. This method is called whenever
